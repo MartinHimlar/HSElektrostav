8c7607331d54edc2470fc08802e62d1d7ded6c15
MartinHimlar <m.himlar@gmail.com>
2015-11-21 11:24:21 +0100

    add fancybox library and refactoring

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
 
 });
+
+$(document).ready(function() {
+	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+});
+
+$(document).ready(function() {
+	$(".fancybox").fancybox();
+});
